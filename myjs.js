282a7659a343036b2cf8e1f1251a00be2459b9c2
manami20051106 <167833068+manami20051106@users.noreply.github.com>
2024-07-09 09:32:27 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,4 +1,11 @@
 alert('Hello,Manami!');
 alert(1+1);
 alert(Math.sqrt(1+1));
-
+alert(Math.sprt(2+3));
+alert(Math.sprt(1/0));
+alert(Math.sprt(0/0));
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
